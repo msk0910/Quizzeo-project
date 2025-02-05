d9efdee1537126a100d9dc938016ddcd5761842e
ckevin randrianirina <ckevin.kalipso@outlook.com>
2025-02-05 18:52:02 +0100

    suppression de fichier obselete

--- user.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-   [_{{{CITATION{{{_1{](https://github.com/wf3-fullstack/montheme/tree/037cfb002d35d2daad8da25cb4fa60a640307f9f/README.md)[_{{{CITATION{{{_2{](https://github.com/allardk/ppe1/tree/67807c5a7b73e9d46c1f3b9fdbc4eb4a367c2fed/sitemap.php)[_{{{CITATION{{{_3{](https://github.com/billadams/php_mysql_program1/tree/968c4beb49edea61a3d3dccbe432ac8a368eb2e6/index.php)
